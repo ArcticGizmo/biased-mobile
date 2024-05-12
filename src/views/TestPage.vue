69b9d7a9899bd58006cb7904dec5163d86f10775
ArcticGizmo <jon.howell314@gmail.com>
2024-05-12 18:13:11 +0800

    better file delete test

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -5,8 +5,10 @@
       <IonButton @click="onLoadKv()">Load KV</IonButton>
       <br />
       <br />
-      <IonButton @click="onSaveAndLoadFile()">Save and Load file</IonButton>
-      <IonButton @click="onDeleteFile()">Delete File</IonButton>
+      <IonButton @click="onSaveFile()">Save File</IonButton>
+      <IonButton :disabled="!filePath" @click="onLoadFile()">Load file</IonButton>
+      <IonButton :disabled="!filePath" @click="onDeleteFile()">Delete File</IonButton>
+      <div>Filepath: {{ filePath }}</div>
     </div>
   </BasePage>
 </template>
@@ -16,10 +18,12 @@ import BasePage from './BasePage.vue';
 import { IonButton } from '@ionic/vue';
 import { KvStore } from '@/composables/kvStore';
 import { FileStore } from '@/composables/fileStore';
+import { ref } from 'vue';
 
 type MyKvData = { egg: number } | { duck: string };
 
 const data = [{ egg: 7 }, { duck: 'lettuce' }];
+const filePath = ref('');
 
 const onSaveKv = async () => {
   await KvStore.saveJson<MyKvData[]>('my-data', data);
@@ -30,17 +34,23 @@ const onLoadKv = async () => {
   console.dir(resp);
 };
 
-const onSaveAndLoadFile = async () => {
+const onSaveFile = async () => {
   const saveResp = await FileStore.save('my-file.txt', 'my fun message');
   if (!saveResp.ok) {
+    console.error('cannot save file', saveResp.error);
     return;
   }
-  console.log('saved', saveResp);
-  const loadResp = await FileStore.load(saveResp.path);
+
+  filePath.value = saveResp.path;
+};
+
+const onLoadFile = async () => {
+  const loadResp = await FileStore.load(filePath.value);
   console.log('loaded', loadResp);
 };
 
 const onDeleteFile = async () => {
-  await FileStore.remove("DATA/my-file.txt")
-}
+  await FileStore.remove(filePath.value);
+  filePath.value = '';
+};
 </script>

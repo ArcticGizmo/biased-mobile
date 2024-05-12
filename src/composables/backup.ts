c0a85cc591b26de0241bb4f5d1d4df8cb485715f
ArcticGizmo <jon.howell314@gmail.com>
2024-05-13 00:11:54 +0800

    ability to save and load a packable backup

---
+++ src/composables/backup.ts
@@ -0,0 +1,69 @@
+import { KPopCard, KPopCardPackable } from '@/types';
+import { FilePicker } from '@capawesome/capacitor-file-picker';
+import { FileStore } from './fileStore';
+import { Directory } from '@capacitor/filesystem';
+
+export type LoadBackupResponse = { ok: true; cards: KPopCardPackable[] } | { ok: false; error: any };
+
+export const createBackup = async (cards: KPopCard[]) => {
+  const backup = await buildBackup(cards);
+  const filename = `kpop-cards-backup-${getBackupName()}.txt`;
+  return await FileStore.save(filename, JSON.stringify(backup), { directory: Directory.Documents });
+};
+
+const buildBackup = async (cards: KPopCard[]) => {
+  const packedCards: KPopCardPackable[] = [];
+
+  for (const card of cards) {
+    const loadResult = await FileStore.loadImage(card.imageFilePath);
+    if (!loadResult.ok) {
+      console.error('could not load file', card.imageFilePath, loadResult.error);
+      throw loadResult.error;
+    }
+
+    packedCards.push({
+      id: card.id,
+      imageSrc: loadResult.data || '',
+      artist: card.artist,
+      artistType: card.artistType,
+      groupName: card.groupName,
+      whereFrom: card.whereFrom,
+      whereFromName: card.whereFromName,
+      albumVersion: card.albumVersion,
+      year: card.year,
+      ownershipType: card.ownershipType
+    });
+  }
+
+  return packedCards;
+};
+
+const padNumber = (num: number) => `${num}`.padStart(2, '0');
+
+const getBackupName = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = padNumber(now.getMonth() + 1);
+  const date = padNumber(now.getDate());
+  const hour = padNumber(now.getHours());
+  const minutes = padNumber(now.getMinutes());
+  const seconds = padNumber(now.getSeconds());
+  return `${year}${month}${date}-${hour}${minutes}${seconds}`;
+};
+
+export const loadBackup = async () => {
+  try {
+    const result = await FilePicker.pickFiles({ types: ['text/plain'], readData: true });
+    const file = result.files[0];
+
+    if (!file) {
+      return { ok: false, error: 'no file selected' };
+    }
+
+    const rawData = atob(file.data as string) || '[]';
+    return { ok: true, data: JSON.parse(rawData) };
+  } catch (error) {
+    console.error('[backup] unable to load backup', error);
+    return { ok: false, error };
+  }
+};
--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -12,11 +12,9 @@
 <script setup lang="ts">
 import { IonButton } from '@ionic/vue';
 import BasePage from './BasePage.vue';
-import { Directory } from '@capacitor/filesystem';
 import { useKPopCards } from '@/composables/kPopCards';
 import { showLoading } from '@/composables/modals';
-import { KPopCardPackable } from '@/types';
-import { FileStore } from '@/composables/fileStore';
+import { createBackup, loadBackup } from '@/composables/backup';
 
 const { cards } = useKPopCards();
 
@@ -24,11 +22,8 @@ const onCreateBackup = async () => {
   const loading = await showLoading();
 
   try {
-    const backup = await buildBackup();
-    console.dir(backup);
-    const filename = `kpop-cards-backup-${getBackupName()}.txt`;
-    await FileStore.save(filename, JSON.stringify(backup), { directory: Directory.Documents });
-    console.dir(filename);
+    const resp = await createBackup([...cards.value]);
+    console.dir(resp);
   } catch (error) {
     console.error(error);
   } finally {
@@ -36,56 +31,8 @@ const onCreateBackup = async () => {
   }
 };
 
-const padNumber = (num: number) => `${num}`.padStart(2, '0');
-
-const getBackupName = () => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = padNumber(now.getMonth() + 1);
-  const date = padNumber(now.getDate());
-  const hour = padNumber(now.getHours());
-  const minutes = padNumber(now.getMinutes());
-  const seconds = padNumber(now.getSeconds());
-  return `${year}${month}${date}-${hour}${minutes}${seconds}`;
-};
-
-const buildBackup = async () => {
-  const packedCards: KPopCardPackable[] = [];
-
-  for (const card of cards.value) {
-    const loadResult = await FileStore.loadImage(card.imageFilePath);
-    if (!loadResult.ok) {
-      console.error('could not load file', card.imageFilePath, loadResult.error);
-      return;
-    }
-
-    packedCards.push({
-      id: card.id,
-      imageSrc: loadResult.data || '',
-      artist: card.artist,
-      artistType: card.artistType,
-      groupName: card.groupName,
-      whereFrom: card.whereFrom,
-      whereFromName: card.whereFromName,
-      albumVersion: card.albumVersion,
-      year: card.year,
-      ownershipType: card.ownershipType
-    });
-  }
-
-  return packedCards;
-};
-
 const onLoadBackup = async () => {
-  await pickFile();
+  const resp = await loadBackup();
+  console.dir(resp);
 };
-
-// const onDownload = async () => {
-//   const resp = await saveFile('egg.json', JSON.stringify(data), Directory.Documents);
-//   console.dir(resp);
-// };
-
-// const onPickFile = async () => {
-//   const resp = await pickFile();
-// };
 </script>

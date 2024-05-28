6918443cc4aacb456c59c75dd218252485320384
ArcticGizmo <jon.howell314@gmail.com>
2024-05-28 21:46:46 +0800

    responsive grid size

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <BasePage title="Home" hide-back-ref>
-    <div class="grid gap-0 grid-cols-3 py-4">
+    <div class="grid gap-0 py-4" :class="colsClass">
       <KCard
         v-for="(card, index) of cards"
         :key="index"
@@ -20,6 +20,28 @@ import { useSimpleRouter } from '@/composables/router';
 import { useKPopCards } from '@/composables/kPopCards';
 import KCard from '@/components/KCard.vue';
 import { FileStore } from '@/composables/fileStore';
+import { useWindowSize } from '@vueuse/core';
+import { computed } from 'vue';
+
+const { width } = useWindowSize();
+
+const colsClass = computed(() => {
+  const w = width.value;
+
+  if (w < 300) {
+    return 'grid-cols-2';
+  }
+
+  if (w < 450) {
+    return 'grid-cols-3';
+  }
+
+  if (w < 900) {
+    return 'grid-cols-4';
+  }
+
+  return 'grid-cols-5';
+});
 
 const router = useSimpleRouter();
 const { cards } = useKPopCards();

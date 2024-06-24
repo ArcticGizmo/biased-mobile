d00e398177c46de7e5981f7a33fae693b2d0f7ca
ArcticGizmo <jon.howell314@gmail.com>
2024-06-24 22:15:35 +0800

    fixed filter item

--- src/components/FilterItem.vue
+++ src/components/FilterItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <IonButton class="filter-item" fill="outline" color="dark" size="small" :class="{ selected: model }" @click="onToggle()">
+  <IonButton class="filter-item" fill="outline" color="dark" size="small" :class="{ selected: modelValue }" @click="onToggle()">
     <IonIcon v-if="icon" slot="start" :icon="icon" />
     {{ text }}
   </IonButton>
@@ -8,17 +8,16 @@
 <script setup lang="ts">
 import { IonIcon, IonButton } from '@ionic/vue';
 
-const model = defineModel<boolean>();
-
-defineProps<{ text: string; icon?: string; selected?: boolean }>();
+const props = defineProps<{ modelValue?: boolean; text: string; icon?: string; selected?: boolean }>();
 
 const emits = defineEmits<{
+  (e: 'update:modelValue', v: boolean): void;
   (e: 'changed', value: boolean): void;
 }>();
 
 const onToggle = () => {
-  const newValue = !model.value;
-  model.value = newValue;
+  const newValue = !props.modelValue;
+  emits('update:modelValue', newValue);
   emits('changed', newValue);
 };
 </script>
@@ -30,8 +29,8 @@ const onToggle = () => {
 }
 
 .filter-item.selected::part(native) {
-  background-color: rgba(var(--ion-color-dark-rgb), 0.1);
-  border-color: rgba(var(--ion-color-dark-rgb), 0.5);
+  background-color: rgba(var(--ion-color-primary-rgb), 0.3);
+  border-color: rgba(var(--ion-color-primary-rgb), 0.5);
   opacity: 1;
 }
 </style>

ee35d5280089f11a653573b028aafceb2eaba8b6
ArcticGizmo <jon.howell314@gmail.com>
2024-07-04 23:02:13 +0800

    move background and selection separately

--- src/views/ExtractorPage.vue
+++ src/views/ExtractorPage.vue
@@ -2,23 +2,27 @@
   <div class="page" style="border: 1px solid orange">
     <div class="nav-bar">
       <IonButton @click="reset()">Reset</IonButton>
-      <IonButton @click="onRemoveAll()">Remove All</IonButton>
-      <IonButton @click="addSelection()">Add Selection</IonButton>
-      <IonButton @click="onDuplicate()">Duplicate</IonButton>
-      <IonButton @click="onTest()">Test</IonButton>
     </div>
     <div class="image-region">
       <div class="editor">
-        <cropper-canvas ref="cropperCanvas" background>
-          <cropper-image ref="cropperImage" :src="src" alt="image to edit" crossOrigin="anonymous" />
-          <cropper-shade hidden />
-          <cropper-handle action="select" plain />
-          <!-- used to move the background image -->
-          <!-- <cropper-handle action="move" plain /> -->
-          <cropper-selection initial-coverage="0.8" initial-aspect-ratio="0.7" outlined movable resizable multiple zoomable keyboard>
+        <cropper-canvas v-if="show" ref="cropperCanvas" background @actionstart="onActionStart" @actionend="onActionEnd">
+          <cropper-image ref="cropperImage" :src="src" alt="image to edit" crossOrigin="anonymous" translatable scalable />
+          <cropper-shade />
+          <cropper-handle id="image-handle" action="move" plain />
+          <cropper-selection
+            ref="cropperSelection"
+            initial-coverage="0.5"
+            initial-aspect-ratio="0.7"
+            outlined
+            movable
+            resizable
+            linked
+            keyboard
+            zoomable
+          >
             <cropper-grid role="grid" covered />
             <cropper-crosshair centered />
-            <cropper-handle action="move" theme-color="rgba(255, 255, 255, 0.35)" />
+            <cropper-handle id="selection-handle" action="move" theme-color="rgba(255, 255, 255, 0.35)" />
             <cropper-handle action="n-resize" />
             <cropper-handle action="e-resize" />
             <cropper-handle action="s-resize" />
@@ -37,83 +41,83 @@
 <script setup lang="ts">
 import 'cropperjs';
 import { IonButton } from '@ionic/vue';
-import { CropperImage, CropperCanvas } from 'cropperjs';
+import { CropperImage, CropperCanvas, CropperShade, CropperGrid, CropperCrosshair, CropperSelection, CropperHandle } from 'cropperjs';
 import { onMounted, onUnmounted, ref } from 'vue';
-import { Extractor } from '@/composables/extractor';
 
-interface CopiedSelection {
-  x: number;
-  y: number;
-  width: number;
-  height: number;
-}
-
+const show = ref(false);
 const src = ref('https://media.karousell.com/media/photos/products/2022/6/20/bts_jimin_pc_1655706038_59617c25_progressive.jpg');
 
 const cropperCanvas = ref<CropperCanvas>();
 const cropperImage = ref<CropperImage>();
-
-const extractor = new Extractor();
-
-let copiedSelection: CopiedSelection | undefined;
+const cropperSelection = ref<CropperSelection>();
 
 const reset = () => {
   setTimeout(() => {
-    cropperImage.value?.$resetTransform();
-    cropperImage.value?.$center('contain');
-  }, 150);
+    show.value = true;
+    setTimeout(() => {
+      cropperSelection.value?.$reset();
+      cropperImage.value?.$resetTransform();
+      cropperImage.value?.$center('contain');
+    }, 10);
+  }, 500);
 };
 
-const onRemoveAll = () => {
-  extractor.clearSelections();
-};
+let yLock: number | undefined;
 
 const onKeyDown = (event: KeyboardEvent) => {
-  if (event.ctrlKey && event.key === 'c') {
-    const selected = extractor.getActiveSelection();
-    if (selected) {
-      copiedSelection = {
-        x: selected.x,
-        y: selected.y,
-        width: selected.width,
-        height: selected.height
-      };
+  if (event.key === 'Shift') {
+    const selection = cropperSelection.value!;
+
+    if (!selection.linked) {
+      yLock ??= selection.y;
+      selection.y = yLock!;
     }
   }
+};
 
-  if (event.ctrlKey && event.key === 'v') {
-    if (copiedSelection) {
-      extractor.addSelection(copiedSelection);
-    }
+const onKeyUp = (event: KeyboardEvent) => {
+  if (event.key === 'Shift') {
+    yLock = undefined;
   }
 };
 
 onMounted(() => {
   reset();
   window.addEventListener('keydown', onKeyDown);
+  window.addEventListener('keyup', onKeyUp);
 });
 
 onUnmounted(() => {
   window.removeEventListener('keydown', onKeyDown);
+  window.removeEventListener('keyup', onKeyUp);
 });
 
-const addSelection = () => extractor.addSelection();
+const onActionStart = (event: any) => {
+  const id = event.detail.relatedEvent.target?.id;
+  const selection = cropperSelection.value;
 
-const onDuplicate = () => {
-  const selected = extractor.getActiveSelection();
-  if (selected) {
-    extractor.duplicateSelection(selected);
+  if (!selection) {
+    return;
+  }
+
+  if (id === 'image-handle') {
+    selection.linked = true;
+  } else if (id === 'selection-handle') {
+    selection.linked = false;
   }
 };
 
-const onTest = () => {
-  // const c = cropperCanvas.value!;
-  // c.$
-  // c.$setAction('selection');
-  const selections = extractor.getSelections();
-  console.dir(selections);
+const onActionEnd = (event: any) => {
+  const id = event.detail.relatedEvent.target?.id;
+  const selection = cropperSelection.value;
 
-  console.log(extractor.getActiveSelection());
+  if (!selection) {
+    return;
+  }
+
+  if (id === 'image-handle' || id === 'selection-handle') {
+    selection.linked = true;
+  }
 };
 </script>
 
